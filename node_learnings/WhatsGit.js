dd1740b7e787b7548419bd702755eca0af9a4e76
AK-Singh-25 <168993543+AK-Singh-25@users.noreply.github.com>
2025-03-05 16:07:41 +0000

    complete WhatsGit

---
+++ node_learnings/WhatsGit.js
@@ -0,0 +1,2 @@
+// What is Node.js ?
+// Node.js is a C++ program which uses Chrome v8 engine to compile javascript into machine code, adds the functionality of file manipulation and lets us create server.
