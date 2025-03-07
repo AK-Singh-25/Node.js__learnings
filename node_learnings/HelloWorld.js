e75075f3dbc4bfa4bed00a02f3493cf8ace5bce9
AK-Singh-25 <168993543+AK-Singh-25@users.noreply.github.com>
2025-03-07 20:57:57 +0530

    complete npm

---
+++ node_learnings/HelloWorld.js
@@ -0,0 +1,10 @@
+console.log("hello world !");
+
+// The second and third line not word in Node.js cuz Dom related functions were removed from V8 engine and only Core Javascript related functions were left intact.
+//  On top of it functionality like file-handling and cryptography were added. 
+
+console.log(Window);
+console.log(alert("heyy"));
+
+// npm - Node package manager
+// "npm init" creates a file called "package.json" which is a configuration file for all the dependencies(external packages) & Scripts
