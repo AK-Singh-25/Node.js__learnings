91462ad4e0b85ce3ae6fe63ef9f946d447601ec5
AK-Singh-25 <168993543+AK-Singh-25@users.noreply.github.com>
2025-03-07 21:43:41 +0530

    complete ModulesInNode

---
+++ node_learnings/ModulesInNode.js
@@ -0,0 +1,6 @@
+// In Node.js, a module is a collection of JavaScript functions and objects that can be used by external applications.
+// It is useful in Modular programming, where a program is divided into modules with specific functions
+
+const math = require("./mathsModule")
+
+console.log(math.sub(2,4));
